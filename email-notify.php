1be4c3f72278d7ee07219fc74aa8bff9134f7465
anubis <spencer.rinehart@dominionenterprises.com>
2008-09-25 15:29:54 -0400

    Change to a model that grabs all headers and the body, so that we can move towards a more complete system.  Still need to work on getting multiple to addresses into the array, but it should work better now.

--- email-notify.php
+++ email-notify.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
-function hypermail_nextid($dir = "/home/anubis/html/email")
+function hypermail_lastid($dir = "/home/anubis/html/email")
 {
 	exec("ls -r $dir/*html", $ids);
 	foreach($ids as $id)
 		if(preg_match("/(\d{4,}).html$/", $id, $match))
-			return $match[1] + 1;
+			return $match[1];
 }
 
 $emailfile = '/home/anubis/projects/email-notify/temp.msg';
@@ -17,18 +17,30 @@ foreach(file($emailfile) as $line)
 	if(!trim($line))
 		$headers_over = true;
 
-	if(!@$headers_over)
+	if(@$headers_over)
+		$body[] = $line;
+	else
 	{
-		//TODO: Long subjects, etc wrap to next line with tabs at beginning of line - lets change this to concatenate.
-		if(substr($line, 0, 5) == 'From:' && (preg_match('/<([^:<]+@[^>]+)>/', $line, $matches) || preg_match('/([^:<]+@[^>]+)/', $line, $matches)))
-			$from = '<b><' . trim($matches[1]) . '></b>';
-		if(substr($line, 0, 8) == 'Subject:')
-			$subject = trim(substr($line, 8));
+		if(preg_match("/^\s+/", $line))
+		{
+			$header = @$prev;
+			$value = trim($line);
+		}
+		else
+		{
+			preg_match("/^([^:]*):(.*)$/", $line, $matches);
+			$prev = $header = strtolower(trim($matches[1]));
+			$value = trim($matches[2]);
+		}
+
+		if(preg_match('/<([^:<]+@[^>]+)>/', $value, $matches) || preg_match('/([^:<]+@[^>]+)/', $value, $matches))
+			$value = trim($matches[1]);
+		$headers[$header][] = $value;
 	}
 }
 
 $to = 'nubs';
-$info = array('<c: 13>email</c>', @$from, '<c: 09>' . @$subject . '</c>', sprintf("http://10.68.4.136/email/%04d.html", hypermail_nextid()));
+$info = array('<c: 13>email</c>', '<b>' . (@$headers['from'][0] == 'spencer.rinehart@dominionenterprises.com' ? "TO: {$headers['to'][0]}" : $headers['from'][0]) . '</b>', '<c: 09>' . implode(' ', @$headers['subject']) . '</c>', sprintf("http://10.68.4.136/email/%04d.html", hypermail_lastid()));
 
 file("http://anubis.homelinux.com:8080/drbplugin_trigger.php?channel=" . urlencode($to) . "&str=" . urlencode(implode(" :: ", $info)));
 ?>
