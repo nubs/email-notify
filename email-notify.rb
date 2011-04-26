a8d37794bba9e30d574aaf152d29c4542013a198
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2011-04-26 13:35:02 -0400

    Rely on verizon to limit the message length.

--- email-notify.rb
+++ email-notify.rb
@@ -6,4 +6,4 @@ require "net/smtp"
 email = Mail.new($stdin.read)
 addresses = (email["received"] || email.from != "spencer.rinehart@dominionenterprises.com" ? "" : "TO: ") + "#{(email["received"] || email.from != "spencer.rinehart@dominionenterprises.com" ? email.from : [email.to, email.cc, email.bcc]).flatten.compact.uniq}"
 
-Net::SMTP.start('smtp.dominionenterprises.com') {|smtp| smtp.send_message "From: anubisnotify\nTo: 7576303572@vtext.com\nSubject: Email\n\n#{"#{email.subject}\n#{addresses}\n#{email.body.decoded}"[0,160]}", "anubis@vt.edu", "7576303572@vtext.com" }
+Net::SMTP.start('smtp.dominionenterprises.com') {|smtp| smtp.send_message "From: anubisnotify\nTo: 7576303572@vtext.com\nSubject: Email\n\n#{"#{email.subject}\n#{addresses}\n#{email.body.decoded}"}", "anubis@vt.edu", "7576303572@vtext.com" }
