eab41feec00e5d3a88afc6b037f080be0eeacc6a
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2011-05-05 10:49:41 -0400

    Ugly fix to make single-part html emails work.

--- email-notify.rb
+++ email-notify.rb
@@ -4,33 +4,40 @@ require "mail"
 email = Mail.new($stdin.read)
 addresses = (email["received"] ? email.from : ['TO', email.to, email.cc, email.bcc]).flatten.compact.uniq
 
-def gettextpart(part)
+def converttotext(part)
+  if part.content_type =~ /^text\/html/
+    IO.popen("elinks -dump -dump-charset utf8 -default-mime-type text/html", "r+") {|io|
+      io.puts part.body
+      io.close_write
+      return io.read
+    }
+  end
+
+  part.body.decoded
+end
+
+def gettext(part)
   if part.multipart?
-    multiparttexts = part.parts.select {|p| p.multipart? }.map {|p| gettextpart(p) }
+    multiparttexts = part.parts.select {|p| p.multipart? }.map {|p| gettext(p) }
 
     alltextparts = multiparttexts + part.parts
 
     plaintextparts = alltextparts.select {|p| p.content_type =~ /^text\/plain/ }
-    return plaintextparts.first unless plaintextparts.empty?
+    return plaintextparts.first.body.decoded unless plaintextparts.empty?
 
     htmltextparts = alltextparts.select {|p| p.content_type =~ /^text\/html/ }
-    return IO.popen("elinks -dump -dump-charset utf8 -default-mime-type text/html", "r+") {|io|
-      io.puts htmltextparts.first.body
-      io.close_write
-      htmltextparts.first.body = io.read
-      htmltextparts.first
-    } unless htmltextparts.empty?
+    return converttotext(htmltextparts.first) unless htmltextparts.empty?
 
     textparts = alltextparts.select {|p| p.content_type =~ /^text\// }
-    return textparts.first unless textparts.empty?
+    return textparts.first.body.decoded unless textparts.empty?
 
     nonmultiparts = alltextparts.select {|p| !p.multipart? }
-    return nonmultiparts.first unless nonmultiparts.empty?
+    return nonmultiparts.first.body.decoded unless nonmultiparts.empty?
 
-    return alltextparts.first
+    return alltextparts.first.body.decoded
   else
-    return part
+    return converttotext(part)
   end
 end
 
-puts "#{email.subject}\n#{addresses.join(",")}\n#{gettextpart(email).body.decoded}"
+puts "#{email.subject}\n#{addresses.join(",")}\n#{gettext(email)}"
